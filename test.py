241709c0e8804a999747adbfcf3571d05b49989b
jrhoads <joseph_rhoads@brown.edu>
2015-06-19 10:00:58 -0400

    Refactor the creation of test IDs.  Split the create section of the API into two tests.  Recognize that DOIs will be returned from the API in uppercase

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import unittest
 import ezid_api
+import uuid
 
 creator = 'ezid_api.py tests'
 
 class EzidApiTests(unittest.TestCase):
-    
+    """Suite of tests for the EzidApi service"""
+
+    def __create_test_id(self):
+        """Create a test ID involving a random component"""
+        return 'PYEZID'+ str(uuid.uuid4())[:8]
+
     def setUp(self):
         self.arkSession = ezid_api.ApiSession()
         self.doiSession = ezid_api.ApiSession(scheme='doi')
@@ -14,22 +20,32 @@ class EzidApiTests(unittest.TestCase):
         self.ids = [self.ark, self.doi]
 
     def test_ark_mint(self):
+        """Mint a new ark id and make sure the resulting string starts wtih the 'ark' scheme"""
         ark = self.arkSession.mint()
         self.assertTrue(ark.startswith(ezid_api.schemes['ark']))
 
-
     def test_doi_mint(self):
+        """Mint a new doi id and make sure the resulting string starts wtih the 'doi' scheme"""
         doi = self.doiSession.mint()
         self.assertTrue(doi.startswith(ezid_api.schemes['doi']))
 
-    def test_create(self):
-        tail = 'PYEZID'
-        ark = self.arkSession.create(tail)
+    def test_ark_create(self):
+        """Create a new EzidRecord who's primary identifier follows the 'ark' scheme"""
+        t_id = self.__create_test_id()
+        ark = self.arkSession.create(t_id)
         self.ids.append(ark)
-        self.assertEqual(ark, ezid_api.schemes['ark'] + ezid_api.testShoulder[ezid_api.schemes['ark']] + tail)
-        doi = self.doiSession.create(tail)
+        self.assertEqual(ark, ezid_api.schemes['ark'] + ezid_api.testShoulder[ezid_api.schemes['ark']] + t_id)
+
+    def test_doi_create(self):
+        """Create a new EzidRecord who's primary identifier follows the 'doi' scheme"""
+        t_id = self.__create_test_id()
+        doi = self.doiSession.create(t_id)
         self.ids.append(doi)
-        self.assertTrue(doi.startswith(ezid_api.schemes['doi'] + ezid_api.testShoulder[ezid_api.schemes['doi']] + tail))
+        self.assertTrue(
+            doi.startswith(
+                ezid_api.schemes['doi'] + ezid_api.testShoulder[ezid_api.schemes['doi']] + t_id.upper()
+            )
+        )
 
     def test_delete(self):
         ark = self.arkSession.mint()
@@ -48,7 +64,6 @@ class EzidApiTests(unittest.TestCase):
         arkGet = self.arkSession.get(self.ark)
         self.assertTrue(arkGet['metadata']['_updated'] > updated)
         self.assertEqual(arkGet['metadata']['dc.creator'], creator)
-        
 
     def test_scheme_setter(self):
         self.assertEqual(self.arkSession.scheme, ezid_api.schemes['ark'])
@@ -73,4 +88,4 @@ class EzidApiTests(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-        
+
